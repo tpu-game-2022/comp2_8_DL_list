fcd5a1770981c19de440b67ad8983d91a0749b23
SWD9 <kawaren623@icloud.com>
2022-11-19 16:10:04 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -23,26 +23,109 @@ void initialize_list(list* l)
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
+	if (l == NULL || p == NULL)
+	{
+		return;
+	}
+
+	if (l->header != NULL)
+	{
+		p->pNext = l->header;
+		l->header->pPrev = p;
+	}
+	else
+	{
+		l->footer = p;
+	}
+
+	l->header = p;
 }
 
 // 末尾にデータを追加
 void push_back(list* l, node* p)
 {
+	if (l == NULL || p == NULL)
+	{
+		return;
+	}
+
+	if (l->footer != NULL)
+	{
+		p->pPrev = l->footer;
+		l->footer->pNext = p;
+	}
+	else
+	{
+		l->header = p;
+	}
+
+	l->footer = p;
 }
 
 // pのノードを削除
 void remove_node(list* l, node* p)
 {
+	if (l == NULL || p == NULL)
+	{
+		return;
+	}
+
+	if (p->pPrev != NULL)
+	{
+		p->pPrev->pNext = p->pNext;
+	}
+	else
+	{
+		l->header = p->pNext;
+	}
+
+	if (p->pNext != NULL)
+	{
+		p->pNext->pPrev = p->pPrev;
+	}
+	else
+	{
+		l->footer = p->pPrev;
+	}
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
+	if (l == NULL || p == NULL || p->pNext == NULL)
+	{
+		return;
+	}
+
+	p->pNext = p->pNext->pNext;
+	if (p->pNext != NULL)
+	{
+		p->pNext->pPrev = p;
+	}
+	else
+	{
+		l->footer = p;
+	}
 }
 
 // pの前のノードを削除
 void remove_prev(list* l, node* p)
 {
+
+	if (l == NULL || p == NULL || p->pPrev == NULL)
+	{
+		return;
+	}
+
+	p->pPrev = p->pPrev->pPrev;
+	if (p->pPrev != NULL)
+	{
+		p->pPrev->pNext = p;
+	}
+	else
+	{
+		l->header = p;
+	}
 }
 
 
