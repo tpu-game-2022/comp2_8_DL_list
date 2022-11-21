4ab23cfa788308ca892a8f1b72edc4e98c1f198a
Mocha0924 <108125975+Mocha0924@users.noreply.github.com>
2022-11-21 14:48:30 +0900

    コミットします

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -23,26 +23,90 @@ void initialize_list(list* l)
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
+	if (l->header != NULL)
+	{
+		p->pNext = l->header;
+		l->header->pPrev = p;
+	}
+	else
+	{
+		l->footer = p;
+	}
+	l->header = p;
 }
 
 // 末尾にデータを追加
 void push_back(list* l, node* p)
 {
+	if (l->footer != NULL)
+	{
+		p->pPrev = l->footer;
+		l->footer->pNext = p;
+	}
+	else
+	{
+		l->header = p;
+	}
+	l->footer = p;
 }
 
 // pのノードを削除
 void remove_node(list* l, node* p)
 {
+	
+	if(p->pPrev!=NULL)
+	{
+		p->pPrev->pNext = p->pNext;
+	}
+	else
+	{
+		l->header = p->pNext;
+	}
+	if (p->pNext != NULL)
+	{
+		p->pNext->pPrev = p->pPrev;
+	}
+	else
+	{
+		l->footer = p->pPrev;
+	}
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
+	if(l->footer==p)
+	{
+		return;
+	}
+	p->pNext = p->pNext->pNext;
+	if(p->pNext!=NULL)
+	{
+			p->pNext->pPrev = p;
+	}
+	else
+	{
+		l->footer = p;
+	}
+	
 }
 
 // pの前のノードを削除
 void remove_prev(list* l, node* p)
 {
+	if (l->header == p)
+	{
+		return;
+	}
+	p->pPrev = p->pPrev->pPrev;
+	if (p->pPrev != NULL)
+	{
+		p->pPrev->pNext = p;
+	}
+	else
+	{
+		l->header = p;
+	}
 }
 
 
